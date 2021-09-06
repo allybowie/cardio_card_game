24fa998de16ec8dd56449ba0d1bab328b875f6b3
allybowie <ally_bowie@hotmail.com>
2021-09-06 21:40:31 +0100

    Set up project. Created deck and card components

---
+++ cardio_card_game/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <h1>Cardio Card Game</h1>
+    <Deck />
+    <input v-for="(exercise, index) in defaultExercises" :key="`input-exercise-${index}`" :v-model="`customExercise${index+1}`" :value="exercise.exercise" :placeholder="exercise.exercise"/>
+  </div>
+</template>
+
+<script>
+import Deck from "./components/Deck/Deck.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Deck
+  },
+  data() {
+    return {
+      defaultExercises: [
+        {
+          suit: "clubs",
+          exercise: "Burpee"
+        },
+        {
+          suit: "hearts",
+          exercise: "Pushup"
+        },
+        {
+          suit: "diamonds",
+          exercise: "Leg Raise"
+        },
+        {
+          suit: "spades",
+          exercise: "Squat"
+        }
+      ],
+      customExercise1: '',
+      customExercise2: '',
+      customExercise3: '',
+      customExercise4: ''
+    }
+  },
+  mounted() {
+    this.setDefaults();
+  },
+  methods: {
+    setDefaults() {
+      this.defaultExercises.forEach((exercise, index) => {
+          this[`customExercise${index+1}`] = exercise.exercise;
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cardio_card_game/src/components/Deck/Deck.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <card />
+    </div>
+</template>
+
+<script>
+import Card from "../Card/Card.vue";
+import cardValues from "../../data/cards.json";
+
+export default {
+    data() {
+        return {
+            cardValues,
+            defaultExercises: [
+                "Burpee",
+                "Pushup",
+                "Leg Raise",
+                "Squat"
+            ],
+            exerciseOne: '',
+            exerciseTwo: '',
+            exerciseThree: '',
+            exerciseFour: '',
+            activeCard: null,
+            finalDeck: []
+        }
+    },
+    components: {
+        Card
+    },
+    props: {
+        customExercises: {
+            type: Array,
+            default: () => []
+        } 
+    },
+    mounted() {
+        this.finalDeck = this.shuffleDeck();
+    },
+    methods: {
+        shuffleDeck() {
+           if(!this.customExercises.length) {
+                let deck = this.defaultExercises.reduce((deckArray, exercise) => {
+                    
+                    let exerciseValues = cardValues.cardValues.map(value => {
+                        return {
+                            value: value,
+                            exercise: exercise
+                        }
+                    })
+                    deckArray.push(exerciseValues);
+                    return deckArray;
+                }, [])
+                
+                return deck.flat()
+            } 
+        }
+    },
+    computed: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
